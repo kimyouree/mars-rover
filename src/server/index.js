99f520f5e8f0b21c929187bc847b878109da64e0
You-Ree Kim <kimyouree@icloud.com>
2021-12-26 12:10:29 -0500

    feat-BE: get manifest & photos requets fetched on the server

--- src/server/index.js
+++ src/server/index.js
@@ -13,50 +13,57 @@ app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname, "../public")));
 
 // your API calls
-// ** NEXT:
-// 1. ON-LOAD: fetch most recent images from each rover
-// 2. how can we get the daily photo AND the gallery images in ONE GET request?
-// 3. how to query Curiosity, Opportunity, Spirit?
-// Curiosity: max_date: "2021-12-16"
-// Opportunity: max_date: "2018-06-11"
-// Spirit: max_date: "2010-03-21"
 app.get("/photos", async (req, res) => {
   try {
-    // *** NEXT: clean up reassignment lines:
-    // 1. make 2 GET calls:
-    //    a. to manifest,
-    //    b. most recent photos by max_date
-    // 2. find out how to chain fetch calls
+    // *** FUTURE: is there a cleaner way to do this? seems wordy
+    // would this be better in a loop - instead of hardcoding each rover's name per call?
     let curiosity_manifest = await fetch(
       `https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity/?api_key=${process.env.API_KEY}`
     ).then((res) => {
-      max_date = res.photo_manifest.max_date;
-      return max_date.json();
+      return res.json();
     });
+    let curiosity_images = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/Curiosity/photos?earth_date=${curiosity_manifest["photo_manifest"]["max_date"]}&api_key=${process.env.API_KEY}`
+    ).then((res) => {
+      return res.json();
+    });
+    // .then((json) => console.log(json));
+
     let opportunity_manifest = await fetch(
       `https://api.nasa.gov/mars-photos/api/v1/manifests/Opportunity/?api_key=${process.env.API_KEY}`
     ).then((res) => {
       return res.json();
     });
+    let opportunity_images = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/Opportunity/photos?earth_date=${opportunity_manifest["photo_manifest"]["max_date"]}&api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+    // .then((json) => console.log(json));
     let spirit_manifest = await fetch(
       `https://api.nasa.gov/mars-photos/api/v1/manifests/Spirit/?api_key=${process.env.API_KEY}`
     ).then((res) => {
       return res.json();
     });
 
-    // manifest = {
-    //   curiosity_images: curiosity_images,
-    //   opportunity_images: opportunity_images["photo_manifest"],
-    //   spirit_images: spirit_images["photo_manifest"],
-    // };
-    // manifest.curiosity_images.photos = manifest.curiosity_images.photos.slice(
-    //   0,
-    //   5
-    // );
-    // manifest.opportunity_images.photos =
-    //   manifest.opportunity_images.photos.slice(0, 5);
-    // manifest.spirit_images.photos = manifest.spirit_images.photos.slice(0, 5);
-    // res.send(manifest);
+    let spirit_images = await fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/Spirit/photos?earth_date=${spirit_manifest["photo_manifest"]["max_date"]}&api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+    // .then((json) => console.log(json));
+
+    rovers_info = {
+      curiosity: {
+        curiosity_manifest,
+        curiosity_images,
+      },
+      opportunity: {
+        opportunity_manifest,
+        opportunity_images,
+      },
+      spirit: {
+        spirit_manifest,
+        spirit_images,
+      },
+    };
+    res.send(rovers_info);
   } catch (err) {
     console.log("error:", err);
   }
